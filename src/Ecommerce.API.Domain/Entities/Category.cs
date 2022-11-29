e8d03eed75c5d41df49d56f56256aeb5812dca9e
darlenecoelho <darle2015@gmail.com>
2022-11-28 23:46:22 -0300

    feat: create first entities

---
+++ src/Ecommerce.API.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.API.Domain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateRegister { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public bool Status { get; set; }
+        public IList<Subcategory> Subcategories { get; set; } = new List<Subcategory>();
+
+    }
+}
---
+++ src/Ecommerce.API.Domain/Entities/Subcategory.cs
@@ -0,0 +1,13 @@
+﻿namespace Ecommerce.API.Domain.Entities
+{
+    public class Subcategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateRegister { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public bool Status { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
