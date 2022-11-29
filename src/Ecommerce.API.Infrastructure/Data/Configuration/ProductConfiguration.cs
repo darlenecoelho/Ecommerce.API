329828537140906be2717508579051902355f668
darlenecoelho <darle2015@gmail.com>
2022-11-29 00:20:23 -0300

    feat: entity mapping in database.

---
+++ src/Ecommerce.API.Infrastructure/Data/Configuration/ProductConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Ecommerce.API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.API.Infrastructure.Data.Configuration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Id)
+                .IsRequired()
+                .ValueGeneratedOnAdd();
+
+            builder.Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
