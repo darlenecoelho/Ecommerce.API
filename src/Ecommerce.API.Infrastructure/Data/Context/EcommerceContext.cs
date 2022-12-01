baffae309bd54ae5861fc733b63584760474ea95
darlenecoelho <darle2015@gmail.com>
2022-11-30 23:04:27 -0300

    feat: create DbContext

---
+++ src/Ecommerce.API.Infrastructure/Data/Context/EcommerceContext.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.API.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System.Reflection;
+
+namespace Ecommerce.API.Infrastructure.Data.Context
+{
+    public class EcommerceContext : DbContext
+    {
+        public EcommerceContext(DbContextOptions<EcommerceContext> dbOptions) : base(dbOptions)
+        {
+            ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Subcategory> Subcategories => Set<Subcategory>();
+        public DbSet<Product> Products => Set<Product>();
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
