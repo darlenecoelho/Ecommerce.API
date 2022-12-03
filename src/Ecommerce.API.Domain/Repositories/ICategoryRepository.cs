d7e325f5567cec2fce575d57a28a9220beafbb01
darlenecoelho <darle2015@gmail.com>
2022-12-03 15:35:48 -0300

    feat: added category repository and interface.

---
+++ src/Ecommerce.API.Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,19 @@
+﻿using Ecommerce.API.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.API.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> ListAsync();
+        Task AddAsync(Category category);
+        Task<Category> FindByIdAsync(int? id);
+        Task<List<Category>> GetByNameAsync(string name);
+        void Update(Category category);
+        void Remove(Category category);
+    }
+}
---
+++ src/Ecommerce.API.Infrastructure/Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,50 @@
+﻿using Ecommerce.API.Domain.Entities;
+using Ecommerce.API.Domain.Repositories;
+using Ecommerce.API.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.API.Infrastructure.Data.Repositories
+{
+    public class CategoryRepository : BaseRepository, ICategoryRepository
+    {
+        public CategoryRepository(EcommerceContext context) : base(context) { }
+
+        public async Task<IEnumerable<Category>> ListAsync()
+        {
+            return await _context.Categories
+                                 .AsNoTracking()
+                                 .ToListAsync();
+        }
+
+        public async Task AddAsync(Category category)
+        {
+            await _context.Categories.AddAsync(category);
+        }
+
+        public async Task<Category> FindByIdAsync(int? id)
+        {
+            return await _context.Categories.FindAsync(id);
+        }
+        public async Task<List<Category>> GetByNameAsync(string name)
+        {
+            return await _context.Categories
+                .Where(category => category.Name.Contains(name))
+                .OrderBy(category => category.Name)
+                .ToListAsync();
+        }
+        public void Update(Category category)
+        {
+            _context.Categories.Update(category);
+        }
+
+        public void Remove(Category category)
+        {
+            _context.Categories.Remove(category);
+        }
+    }
+}
