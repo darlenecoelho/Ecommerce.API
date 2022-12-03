180a2c4d29b2351d2553b21b41063ed79c13409b
darlenecoelho <darle2015@gmail.com>
2022-12-03 15:34:42 -0300

    feat: create repository base

---
+++ src/Ecommerce.API.Infrastructure/Data/Repositories/BaseRepository.cs
@@ -0,0 +1,14 @@
+﻿using Ecommerce.API.Infrastructure.Data.Context;
+
+namespace Ecommerce.API.Infrastructure.Data.Repositories
+{
+    public abstract class BaseRepository
+    {
+        protected readonly EcommerceContext _context;
+
+        protected BaseRepository(EcommerceContext context)
+        {
+            _context = context;
+        }
+    }
+}
