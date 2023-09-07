2f25076812119d6645081ce6747c17d0bcfd165a
darlenecoelho <darle2015@gmail.com>
2023-09-06 22:45:55 -0300

    feat: adicionado logger

--- src/Ecommerce.API.Application/Queries/Category/GetAllCategoriesQueryHandler.cs
+++ src/Ecommerce.API.Application/Queries/Category/GetAllCategoriesQueryHandler.cs
@@ -2,6 +2,7 @@
 using Ecommerce.API.Application.DTOs.Category;
 using Ecommerce.API.Domain.Repositories.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Ecommerce.API.Application.Queries.Category;
 
@@ -9,16 +10,23 @@ public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuer
 {
     private readonly ICategoryRepository _categoryRepository;
     private readonly IMapper _mapper;
+    private readonly ILogger<GetAllCategoriesQueryHandler> _logger;
 
-    public GetAllCategoriesQueryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+    public GetAllCategoriesQueryHandler(ICategoryRepository categoryRepository, IMapper mapper, ILogger<GetAllCategoriesQueryHandler> logger)
     {
         _categoryRepository = categoryRepository;
         _mapper = mapper;
+        _logger = logger;
     }
 
     public async Task<List<ReadCategoryDTO>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
     {
+        _logger.LogInformation("Buscando todas as categorias");
+
         var categories = await _categoryRepository.GetAllCategoriesAsync();
+
+        _logger.LogInformation("Total de categorias obtidas: {Count}", categories.Count);
+
         return _mapper.Map<List<ReadCategoryDTO>>(categories);
     }
 }
